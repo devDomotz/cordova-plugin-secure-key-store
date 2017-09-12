fcc66d0c640bbe301a552e0a39a7ad9ec67a5f9a
devDomotz <devDomotz@users.noreply.github.com>
2017-09-12 13:56:18 +0200

    error fix

--- src/ios/SecureKeyStore.m
+++ src/ios/SecureKeyStore.m
@@ -6,9 +6,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -31,8 +31,8 @@
     NSData *serializedDict = [NSPropertyListSerialization dataFromPropertyList:dict format:NSPropertyListXMLFormat_v1_0 errorDescription:&error];
     [keychain setObject:serializedDict forKey:(__bridge id)(kSecValueData)];
     if (error) {
-        NSLog(@"%@", error);
-    }
+    NSLog(@"%@", error);
+}
 }
 
 - (NSMutableDictionary *) readFromSecureKeyStore
@@ -42,39 +42,39 @@
     KeychainItemWrapper * keychain = [[KeychainItemWrapper alloc] initWithIdentifier:@"cordova.plugins.SecureKeyStore" accessGroup:nil];
     NSError *error;
     @try
-    {
-        NSData *serializedDict = [keychain objectForKey:(__bridge id)(kSecValueData)];
-        NSUInteger dictLength = [serializedDict length];
-        if (dictLength) {
-            // de-serialize dictionary
-            dict = [NSPropertyListSerialization propertyListFromData:serializedDict mutabilityOption:NSPropertyListImmutable format:nil errorDescription:&error];
-            if (error) {
-                NSLog(@"Read process Exception: %@", error);
-            }
-        }
-    }
+{
+    NSData *serializedDict = [keychain objectForKey:(__bridge id)(kSecValueData)];
+    NSUInteger dictLength = [serializedDict length];
+    if (dictLength) {
+    // de-serialize dictionary
+    dict = [NSPropertyListSerialization propertyListFromData:serializedDict mutabilityOption:NSPropertyListImmutable format:nil errorDescription:&error];
+    if (error) {
+    NSLog(@"Read process Exception: %@", error);
+}
+}
+}
     @catch (NSException * exception)
-    {
-        NSLog(@"Read exception: %@", exception);
-    }
+{
+    NSLog(@"Read exception: %@", exception);
+}
     return dict;
 }
 
 - (BOOL) removeKeyFromSecureKeyStore:(NSString*) key
 {
     @try
-    {
-        // get mutable dictionary and remove key from store
-        NSMutableDictionary *dict = [self readFromSecureKeyStore];
-        [dict removeObjectForKey:key];
-        [self writeToSecureKeyStore:dict];
-        return YES;
-    }
+{
+    // get mutable dictionary and remove key from store
+    NSMutableDictionary *dict = [self readFromSecureKeyStore];
+    [dict removeObjectForKey:key];
+    [self writeToSecureKeyStore:dict];
+    return YES;
+}
     @catch (NSException * exception)
-    {
-        NSLog(@"Remove exception: %@", exception.reason);
-        return NO;
-    }
+{
+    NSLog(@"Remove exception: %@", exception.reason);
+    return NO;
+}
 }
 
 - (void) resetSecureKeyStore
@@ -85,10 +85,10 @@
 - (void)handleAppUninstallation
 {
     if (![[NSUserDefaults standardUserDefaults] boolForKey:@"RnSksIsAppInstalled"]) {
-        [self resetSecureKeyStore];
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"RnSksIsAppInstalled"];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    }
+    [self resetSecureKeyStore];
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"RnSksIsAppInstalled"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
 }
 
 - (void) set:(CDVInvokedUrlCommand*)command
@@ -98,34 +98,34 @@
     NSString* value = [command.arguments objectAtIndex:1];
 
     @try {
-        // handle app uninstallation
-        [self handleAppUninstallation];
-        // get mutable dictionary and store data
-        [self.commandDelegate runInBackground:^{
-            @synchronized(self) {
-                @try {
-                    NSMutableDictionary *dict = [self readFromSecureKeyStore];
-                    [dict setValue: value forKey: key];
-                    [self writeToSecureKeyStore:dict];
+    // handle app uninstallation
+    [self handleAppUninstallation];
+    // get mutable dictionary and store data
+    [self.commandDelegate runInBackground:^{
+    @synchronized(self) {
+    @try {
+    NSMutableDictionary *dict = [self readFromSecureKeyStore];
+    [dict setValue: value forKey: key];
+    [self writeToSecureKeyStore:dict];
 
-                    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"key saved successfully"];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                }
-                @catch (NSException* exception)
-                {
-                    NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":9,\"message\":\"error saving key, please try to un-install and re-install app again\",\"actual-error\":%@}", exception];
-                    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                }
-            }
-        }];
-    }
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"key saved successfully"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
     @catch (NSException* exception)
-    {
-        NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":9,\"message\":\"error saving key\",\"actual-error\":%@}", exception];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
+{
+    NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":9,\"message\":\"error saving key, please try to un-install and re-install app again\",\"actual-error\":%@}", exception];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+}
+}];
+}
+    @catch (NSException* exception)
+{
+    NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":9,\"message\":\"error saving key\",\"actual-error\":%@}", exception];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
 }
 
 - (void) get:(CDVInvokedUrlCommand*)command
@@ -134,43 +134,44 @@
     NSString* key = [command.arguments objectAtIndex:0];
 
     @try {
-        // handle app uninstallation
-        [self handleAppUninstallation];
-        [self.commandDelegate runInBackground:^{
-            @synchronized(self) {
-             @try {
-                    // get mutable dictionaly and retrieve store data
-                    NSMutableDictionary *dict = [self readFromSecureKeyStore];
-                    NSString *value = nil;
+    // handle app uninstallation
+    [self handleAppUninstallation];
+    [self.commandDelegate runInBackground:^{
+    @synchronized(self) {
+    @try {
+    // get mutable dictionaly and retrieve store data
+    NSMutableDictionary *dict = [self readFromSecureKeyStore];
+    NSString *value = nil;
 
-                    if (dict != nil) {
-                        value =[dict valueForKey:key];
-                    }
+    if (dict != nil) {
+    value =[dict valueForKey:key];
+}
 
-                    if (value != nil) {
-                        value =[dict valueForKey:key];
-                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:value];
-                        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                    } else {
-                        NSString* errorMessage = @"{\"code\":1,\"message\":\"key does not present\"}";
-                        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-                        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                    }
-                }
-            }
-           @catch (NSException* exception){
-              NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":1,\"message\":\"key does not present\",\"actual-error\":%@}", exception];
-              CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-              [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-           }
-        }];
-    }
+    if (value != nil) {
+    value =[dict valueForKey:key];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:value];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+} else {
+    NSString* errorMessage = @"{\"code\":1,\"message\":\"key does not present\"}";
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+}
+    @catch (NSException* exception){
+    NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":1,\"message\":\"key does not present\",\"actual-error\":%@}", exception];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+}
+
+}];
+}
     @catch (NSException* exception)
-    {
-        NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":1,\"message\":\"key does not present\",\"actual-error\":%@}", exception];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
+{
+    NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":1,\"message\":\"key does not present\",\"actual-error\":%@}", exception];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
 }
 
 - (void) remove:(CDVInvokedUrlCommand*)command
@@ -178,27 +179,27 @@
     CDVPluginResult* pluginResult = nil;
     NSString* key = (NSString*)[command.arguments objectAtIndex:0];
     @try {
-        // handle app uninstallation
-        [self handleAppUninstallation];
-        [self.commandDelegate runInBackground:^{
-            @synchronized(self) {
-                BOOL status = [self removeKeyFromSecureKeyStore:key];
-                if (status) {
-                    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Key removed successfully"];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                } else {
-                    NSString* errorMessage = @"{\"code\":6,\"message\":\"could not delete key\"}";
-                    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                }
-            }
-        }];
-    }
+    // handle app uninstallation
+    [self handleAppUninstallation];
+    [self.commandDelegate runInBackground:^{
+    @synchronized(self) {
+    BOOL status = [self removeKeyFromSecureKeyStore:key];
+    if (status) {
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Key removed successfully"];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+} else {
+    NSString* errorMessage = @"{\"code\":6,\"message\":\"could not delete key\"}";
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+}
+}];
+}
     @catch(NSException *exception) {
-        NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":6,\"message\":\"could not delete key\",\"actual-error\":%@}", exception];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }
+    NSString* errorMessage = [NSString stringWithFormat:@"{\"code\":6,\"message\":\"could not delete key\",\"actual-error\":%@}", exception];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errorMessage];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
 }
 
 @end
